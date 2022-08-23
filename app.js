8a5585caed937262da3737a84fc2a2a9505df4d0
Vladimir Solovyov <leilyngale@icloud.com>
2022-08-23 08:19:37 +0300

    Set up basic routes including 404

--- app.js
+++ app.js
@@ -3,18 +3,20 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+//Custom routes imports
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/users', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add user</button></form>');
-  });
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
+app.use((req, res, next) => {
+    res.status(404).send('<h1>404 - Page Not Found</h1>');
 })
-  app.use('/', (req, res, next) => {
-    res.send('<h1>Home page</h1>');
-  });
+
+
+
 
 app.listen(3000);
